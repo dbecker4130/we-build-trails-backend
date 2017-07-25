a787128245a27c88e22da96e141506089f010e2a
Daniel Becker <dbecker4130@gmail.com>
2017-07-25 15:48:29 -0700

    added build badge to README, toggle server helper file complete

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+
+[![Build Status](https://travis-ci.org/dbecker4130/we-build-trails-backend.svg?branch=master)](https://travis-ci.org/dbecker4130/we-build-trails-backend)
+
 # we-build-trails-backend
 A web blog where BMX and mountain bikers can post the process of building, maintaining, and riding a set of trails
---
+++ test/lib/toggle-server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const debug = require('debug')('we-build-trails-backend:server-toggle');
+
+module.exports = exports = {};
+//NOTE exporting both methods as an object
+
+exports.serverOn = function(server, done) {
+  if (!server.isRunning) {
+    server.listen(process.env.PORT, err => {
+      if (err) return done(err);
+      server.isRunning = true;
+      debug(`SERVER UP: ${process.env.PORT}`);
+      done();
+    });
+    return;
+  }
+  done();
+};
+
+exports.serverOff = function(server, done) {
+  if (server.isRunning) {
+    server.close(err => {
+      if (err) return done(err);
+      server.isRunning = false;
+      debug('SERVER DOWN');
+      done();
+    });
+    return;
+  }
+  done();
+};
