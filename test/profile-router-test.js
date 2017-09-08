5423cad74c3e2bda8b556188c5899e1f0365afb0
Daniel Becker <dbecker4130@gmail.com>
2017-09-08 13:58:48 -0700

    GET test for profile router passes

--- test/profile-router-test.js
+++ test/profile-router-test.js
@@ -4,17 +4,15 @@ require('./lib/test-env.js');
 
 const expect = require('chai').expect;
 const request = require('superagent');
-const mongoose = require('mongoose');
 const User = require('../model/user.js');
+
 const server = require('../server.js');
 const serverToggle = require('./lib/toggle-server.js');
 const clearDB = require('./lib/clearDB.js');
+const testData = require('./lib/test-data.js');
 
 const url = `http://localhost:${process.env.PORT}`;
 
-mongoose.Promise = Promise;
-
-const testData = require('./lib/test-data.js');
 const exampleUser = testData.exampleUser;
 const exampleImage = testData.exampleImage;
 
@@ -27,31 +25,26 @@ describe('Profile Routes', function() {
     serverToggle.serverOff(server, done);
   });
   afterEach(done => clearDB(done));
-
-  after( done => {
-    Promise.all([
-      User.remove({})
-    ])
-    .then( () => done())
+  beforeEach(done => {
+    new User (exampleUser)
+    .generatePasswordHash(exampleUser.password)
+    .then(user => user.save())
+    .then(user => {
+      this.tempUser = user;
+      return user.generateToken();
+    })
+    .then(token => {
+      this.tempToken = token;
+      exampleUser.userID = this.tempUser._id.toString();
+      done();
+    })
     .catch(done);
   });
 
+
+
   describe('POST: /api/profile/:userID/image', () => {
     describe('with a valid token and data', () => {
-
-      before( done => {
-        new User(exampleUser)
-        .generatePasswordHash(exampleUser.password)
-        .then( user => user.save())
-        .then( user => {
-          this.tempUser = user;
-          return user.generateToken();
-        })
-        .then( token => {
-          this.tempToken = token;
-          done();
-        });
-      });
 
       it('should return an image', done => {
         request.post(`${url}/api/profile/${this.tempUser._id}/image`)
@@ -72,23 +65,6 @@ describe('Profile Routes', function() {
   describe('PUT: /api/profile/:userID', () => {
     describe('with a valid Body', () => {
 
-      before( done => {
-        new User(exampleUser)
-        .generatePasswordHash(exampleUser.password)
-        .then( user => user.save())
-        .then( user => {
-          this.tempUser = user;
-          return user.generateToken();
-        })
-        .then( token => {
-          this.tempToken = token;
-          done();
-        })
-        .catch(done);
-      });
-      afterEach(() => {
-        delete exampleUser.username;
-      });
 
       it('should update the user', done => {
         let updated = { username: 'new name' };
@@ -108,38 +84,20 @@ describe('Profile Routes', function() {
     });
   });
 
-  describe('GET: /api/profile/:userID', function() {
-    describe('with a VALID body', function() {
+  describe('GET: /api/profile/:userID', () => {
+    describe('with a VALID body', () => {
 
-      before( done => {
-        new User(exampleUser)
-        .generatePasswordHash(exampleUser.password)
-        .then( user => user.save())
-        .then( user => {
-          this.tempUser = user;
-          return user.generateToken();
+      it('should return user data', done => {
+        request.get(`${url}/api/profile/${this.tempUser._id}`)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
         })
-        .then( token => {
-          this.tempToken = token;
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
           done();
-        })
-        .catch(done);
+        });
       });
-      afterEach(() => {
-        delete exampleUser._id;
-      });
-
-      // it('should return user data', done => {
-      //   request.get(`${url}/api/profile/${this.tempUser._id}`)
-      //   .set({
-      //     Authorization: `Bearer ${this.tempToken}`
-      //   })
-      //   .end((err, res) => {
-      //     if (err) return done(err);
-      //     expect(res.status).to.equal(200);
-      //     done();
-      //   });
-      // });
     });
   });
 
